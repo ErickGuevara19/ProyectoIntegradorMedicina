25465344ae7b44c3228e1bbf7b629c3b4b467c65
Jair Delgado <jar.delgado@yavirac.edu.ec>
2023-01-24 21:57:58 -0500

    Pages Router

--- src/app/pages/pages--routing.module.ts
+++ src/app/pages/pages--routing.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardDoctorsComponent } from './dashboard-doctors/dashboard-doctors.component';
+import { DashboardPatientsComponent } from './dashboard-patients/dashboard-patients.component';
+import { PagesComponent } from './pages.component';
+import { AuthmoduleModule } from '../auth/authmodule.module';
 
 
-
+const routes: Routes = [
+  {
+    path: 'dashboard', component: PagesComponent,
+    children: [
+      { path:'', component: DashboardDoctorsComponent},
+      { path: 'doctors', component: DashboardDoctorsComponent},
+      { path: 'patients', component: DashboardPatientsComponent },
+      { path: 'doctors-token', component: DashboardPatientsComponent },
+      { path: 'medical-appoinment-list', component: DashboardPatientsComponent },
+      { path: 'medical-appoinment-selection', component: DashboardPatientsComponent },
+      { path: 'patients-token', component: DashboardPatientsComponent },
+      {path:'',redirectTo:'/dashboard',pathMatch:'full'},
+    ]
+  }
+]
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    AuthmoduleModule,
   ]
 })
 export class PagesRoutingModule { }
