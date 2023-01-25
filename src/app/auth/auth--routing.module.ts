e5aacc3fadbf34b29d238a29d73d2e9d1173ae8a
ErickGuevara19 <ead.guevara@yavirac.edu.ec>
2023-01-24 21:04:12 -0500

    enrutado auth

--- src/app/auth/auth--routing.module.ts
+++ src/app/auth/auth--routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterPatientsComponent } from './register-patients/register-patients.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes :Routes =[
+  {path:'login',component:LoginComponent},
+  {path:'registerPatient',component:RegisterPatientsComponent},
 
+]
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports:[
+    RouterModule
   ]
 })
 export class AuthRoutingModule { }
