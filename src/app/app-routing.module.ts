74ea2fd43e1c9bbca19490473df0de6287d4b9a9
JimmyVin <jimmy-dragon1@hotmail.com>
2023-01-24 21:15:21 -0500

    app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {  Routes } from '@angular/router';
+import {  RouterModule, Routes } from '@angular/router';
+import { NoFoundpagesComponent } from './noFound/no-foundpages.component';
 
+const routes :Routes =[
+
+  {path:'',redirectTo:'dashboard',pathMatch:'full'},
+  {path:'**',component:NoFoundpagesComponent},
+
+
+]
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-  ]
+    AuthRoutingModule,
+    PagesRoutingModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
