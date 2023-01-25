07c59d2c3608095491741bb9d4b56588ca992f78
ErickGuevara19 <ead.guevara@yavirac.edu.ec>
2023-01-24 21:19:57 -0500

    cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {  RouterModule, Routes } from '@angular/router';
 import { NoFoundpagesComponent } from './noFound/no-foundpages.component';
+import { AuthRoutingModule } from './auth/auth--routing.module';
+import { PagesRoutingModule } from './pages/pages--routing.module';
 
 const routes :Routes =[
 
