90379fc9b74aad80b3259da875ef7086e32e7fa6
JimmyVin <jimmy-dragon1@hotmail.com>
2023-01-24 20:38:59 -0500

    bing bang

--- src/app/shared/sharedmodule.module.ts
+++ src/app/shared/sharedmodule.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedComponent } from './shared.component';
+import { SidebarComponent } from "./sidebar/sidebar.component";
 
 
 
 @NgModule({
-  declarations: [
-    SharedComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        SharedComponent
+    ],
+    imports: [
+        CommonModule,
+        SidebarComponent
+    ]
 })
 export class SharedmoduleModule { }
