4799648ad35f46867d499d6b5c29abc627262810
tz <tzcristhiangt90@gmail.com>
2023-01-25 21:38:16 -0500

    Instalación libreria ng-select y desarrollo registro de doctores. Isak

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DoctorsRegisterComponent } from './doctors-register/doctors-register.component';
 import { MedicalAppointmentRegistrationComponent } from './medical-appointment-registration/medical-appointment-registration.component';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 
 
@@ -9,6 +10,7 @@ import { MedicalAppointmentRegistrationComponent } from './medical-appointment-r
   declarations: [DoctorsRegisterComponent,MedicalAppointmentRegistrationComponent],
   imports: [
     CommonModule,
+    NgSelectModule
   ],
   exports: [
     DoctorsRegisterComponent,MedicalAppointmentRegistrationComponent
--- src/app/admin/doctors-register/doctors-register.component.ts
+++ src/app/admin/doctors-register/doctors-register.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./doctors-register.component.css']
 })
 export class DoctorsRegisterComponent {
-
+  specialities: any[] = []
 }
