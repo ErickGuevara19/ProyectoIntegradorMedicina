5b2cbf51115db2d85caff3e782889681d0760d51
tz <tzcristhiangt90@gmail.com>
2023-01-25 21:50:53 -0500

    Funcionalidad doctors register. Isak

--- src/app/admin/doctors-register/doctors-register.component.ts
+++ src/app/admin/doctors-register/doctors-register.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,Inject } from '@angular/core';
+import { DoctorModel } from 'src/app/entities/doctor.model';
+import { DoctorsService } from 'src/app/services/doctors.service';
 
 @Component({
   selector: 'app-doctors-register',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./doctors-register.component.css']
 })
 export class DoctorsRegisterComponent {
+  private doctorService = Inject(DoctorsService)
   specialities: any[] = []
+  doctors: DoctorModel[] = [];
+
+  //async getDoctors(){
+  //  const response = this.doctorService.getAll().subscribe((response) => {
+  //    this.doctors = response;
+  //    console.log(response);
+  //  });
+  //}
+  //getDoctor(id: number) {
+  //  const response = this.doctorService.getOne(id).subscribe((response) => {
+  //    console.log(response);
+  //  });
+  //}
+  //registerDoctor(doctor: DoctorModel) {
+  //  const response = this.doctorService
+  //    .store(doctor)
+  //    .subscribe((response) => {
+  //      console.log(response);
+  //    });
+  //}
+  //updateDoctor(doctor: DoctorModel) {
+  //  const response = this.doctorService
+  //    .update(doctor.id, doctor)
+  //    .subscribe((response) => {
+  //      console.log(response);
+  //    });
+  //}
+  //deleteDoctor(id: number) {
+  //  const response = this.doctorService.destroy(id).subscribe((response) => {
+  //    this.doctors = this.doctorService.filter(doctor => doctor.id != id)
+  //    console.log(response);
+  //  });
+  //}
 }
