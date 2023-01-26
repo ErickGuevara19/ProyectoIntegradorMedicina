c0ea6a9f09dcbf3ae02a655f039a38024f2d433a
ErickGuevara19 <ead.guevara@yavirac.edu.ec>
2023-01-25 21:26:24 -0500

    servicios con inyeccion

--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -1,9 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientService {
+  
+  readonly API_URL ='http://localhost:3000';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 }
