05c6bfa3b660063bd8c546535124a56f12ac7b79
ErickGuevara19 <ead.guevara@yavirac.edu.ec>
2023-02-04 11:35:04 -0500

    cambios

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  constructor(){
+
+  }
+
+  ngOnInit():void{
+
+  }
+
+  /*login(form:NgForm){
+
+    const email = form.value.email;
+    const password = form.value.password
+  }*/
+
 
 }
--- src/app/pipes/pipelogin.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PipeloginPipe } from './pipelogin.pipe';
-
-describe('PipeloginPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PipeloginPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
